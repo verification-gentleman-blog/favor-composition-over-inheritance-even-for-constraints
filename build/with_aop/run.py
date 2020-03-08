2326aa16ba3877e5d79e772bd2ce7b6e33ee1a09
Tudor Timi <tudortimi@users.noreply.github.com>
2020-03-08 15:20:59 +0100

    Add build infrastructure for AOP

---
+++ build/with_aop/run.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+
+import argparse
+import os
+import subprocess
+
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('test')
+parser.add_argument('--gui', '-g', action='store_true')
+args = parser.parse_args()
+
+cmd = [
+    'specman',
+    '-p', "load {}".format(os.path.join(root_dir, 'src/with_aop/e', args.test)),
+    ]
+
+if args.gui:
+    cmd.extend(['-gui'])
+else:
+    cmd.extend(['-c', 'test'])
+
+subprocess.check_call(cmd)
